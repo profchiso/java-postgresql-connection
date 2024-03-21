dab5eb48b3aba9f8590a9cf04d2f552458307f51
Chinedu Okorie <okoriechinedusunday@gmail.com>
2024-03-21 20:38:40 +0100

    Feat: implement insert

--- src/Users.java
+++ src/Users.java
@@ -12,25 +12,30 @@ public class Users {
       * close
       * */
         String url="jdbc:postgresql://localhost:5432/testDB?user=postgres&password=postgres";
-        String query = "SELECT * FROM public.users";
+        String query = "SELECT * FROM users where user_id = 2";
+        String insert = "INSERT into users values ('ken2',true,50,5)";
         Class.forName("org.postgresql.Driver");
         try {
             Connection con = DriverManager.getConnection(url);
-           Statement queryStatement = con.createStatement();
-            ResultSet rs =    queryStatement.executeQuery(query);
+            Statement queryStatement = con.createStatement();
+
+            boolean isInserted= queryStatement.execute(insert);
+
+
+            //ResultSet rs =    queryStatement.executeQuery(query);
 //            rs.next();
 //            System.out.println(rs.getString("name"));
 //            rs.next();
 //            System.out.println(rs.getString("name"));
-            while (rs.next()){
-                System.out.println("Name" +" = " + rs.getString(1));
-                System.out.println("IsMarried" +" = " + rs.getBoolean(2));
-                System.out.println("Age" +" = " + rs.getInt(3));
-                System.out.println("userId" +" = " + rs.getInt(4));
-                System.out.println();
-                System.out.println("****** next user *****");
-                System.out.println();
-            }
+//            while (rs.next()){
+//                System.out.println("Name" +" = " + rs.getString(1));
+//                System.out.println("IsMarried" +" = " + rs.getBoolean(2));
+//                System.out.println("Age" +" = " + rs.getInt(3));
+//                System.out.println("userId" +" = " + rs.getInt(4));
+//                System.out.println();
+//                System.out.println("****** next user *****");
+//                System.out.println();
+//            }
 
             con.close();
 
