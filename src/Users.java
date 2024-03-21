9aa3628dddb5194485c355a8538892a074a53e2d
Chinedu Okorie <okoriechinedusunday@gmail.com>
2024-03-21 19:46:20 +0100

    Feat: implement db connection

---
+++ src/Users.java
@@ -0,0 +1,41 @@
+import java.sql.*;
+
+public class Users {
+    public static void main(String [] args) throws ClassNotFoundException {
+
+      /*
+      * import package
+      * load and register
+      * create connection
+      * execute statement
+      * process the result
+      * close
+      * */
+        String url="jdbc:postgresql://localhost:5432/testDB?user=postgres&password=postgres";
+        String query = "SELECT * FROM public.users";
+        Class.forName("org.postgresql.Driver");
+        try {
+            Connection con = DriverManager.getConnection(url);
+           Statement queryStatement = con.createStatement();
+            ResultSet rs =    queryStatement.executeQuery(query);
+//            rs.next();
+//            System.out.println(rs.getString("name"));
+//            rs.next();
+//            System.out.println(rs.getString("name"));
+            while (rs.next()){
+                System.out.println("Name" +" = " + rs.getString(1));
+                System.out.println("IsMarried" +" = " + rs.getBoolean(2));
+                System.out.println("Age" +" = " + rs.getInt(3));
+                System.out.println("userId" +" = " + rs.getInt(4));
+                System.out.println();
+                System.out.println("****** next user *****");
+                System.out.println();
+            }
+
+            con.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
